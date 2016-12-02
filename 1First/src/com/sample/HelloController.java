dc67e2823d11dd1dda8b640a9ad1f75274b39927
Vijay T <vijay.thoguru@kohls.com>
2016-12-03 01:16:56 +0530

    Changed the same with Annotations writing an annotation based controller class -@Controller, @RequestMapping

--- 1First/src/com/sample/HelloController.java
+++ 1First/src/com/sample/HelloController.java
@@ -3,6 +3,8 @@ package com.sample;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
@@ -11,14 +13,15 @@ import org.springframework.web.servlet.mvc.AbstractController;
  * @author vthoguru
  *
  */
-public class HelloController extends AbstractController{
+@Controller
+public class HelloController{
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
+	@RequestMapping("/helloIndex")
+	public ModelAndView handleRequestInternal(HttpServletRequest arg0,
 			HttpServletResponse arg1) throws Exception {
 //		ModelAndView modelAndView = new ModelAndView(); // if not specified, .html name
 		ModelAndView modelAndView = new ModelAndView("hello"); // name of the jsp
-		modelAndView.addObject("msg", "hello from MVC"); 
+		modelAndView.addObject("msg", "Hello from MVC with Annotation"); 
 		return modelAndView;
 	}
 	
